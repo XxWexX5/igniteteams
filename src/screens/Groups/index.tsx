b925eb24c3c2b34d3997b1447ffa09acfd584575
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-01-26 13:58:55 -0300

    [ FEATURE ] Adding design token.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export function Groups() {
   return (
     <View>
-      <Text className="text-white">Groups</Text>
+      <Text>Groups</Text>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+import { colors, fontSize } from './src/theme'
+
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+      fontSize
+    },
   },
   plugins: [],
 }
