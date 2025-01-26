ce97f9652d319de34afdd70b126a0771ef6a1ecc
Wevison Ramalho Silva <wevison.silva@altoqi.com.br>
2025-01-25 22:51:14 -0300

    [ FEATURE ] Adding nativeWind.

---
+++ src/app/_layout.tsx
@@ -0,0 +1,40 @@
+import { useFonts } from "expo-font";
+
+import * as SplashScreen from "expo-splash-screen";
+
+import { useEffect } from "react";
+
+import "react-native-reanimated";
+
+import { Groups } from "../components/Groups";
+
+import "@/src/styles/global.css";
+import { SafeAreaView, StatusBar } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <StatusBar />
+
+      <SafeAreaView className="w-screen h-screen justify-center items-center">
+        <Groups />
+      </SafeAreaView>
+    </>
+  );
+}
